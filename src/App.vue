b18db88ecd3a8391463506b92ba01cee1bdb6752
Nestor Umboni <umboninestor@gmail.com>
2025-10-01 20:27:47 +0200

    Proyecto Inicial

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+ const titulo = ref<string>("Proyecto VUE con TypeScript");
+ 
+</script>
+
+<template>
+<h1>{{titulo}} </h1>
+</template>
+<style scoped>
+ 
+</style>
+
